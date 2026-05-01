1a8beea1fa634dcc8f190bc7bcb3640db4fb86f3
Vladimir Moiseenko <waldemar.moiseenko@gmail.com>
2026-05-01 22:59:47 +0300

    chore: upgrade to Kotlin 2.0, AGP 8.7.2 and setup Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    val room_version = "2.8.4"
+    val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:2.6.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
